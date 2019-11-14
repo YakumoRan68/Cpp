f7d74e36d9eaa8d86bec979f3b2b279111643e71
YakumoRan <yys14239103@gmail.com>
2019-11-14 09:37:01 +0900

    1114

---
+++ 1114/1114/1114View.h
@@ -0,0 +1,49 @@
+
+// 1114View.h : CMy1114View 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CMy1114View : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CMy1114View();
+	DECLARE_DYNCREATE(CMy1114View)
+
+// 특성입니다.
+public:
+	CMy1114Doc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CMy1114View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // 1114View.cpp의 디버그 버전
+inline CMy1114Doc* CMy1114View::GetDocument() const
+   { return reinterpret_cast<CMy1114Doc*>(m_pDocument); }
+#endif
+
